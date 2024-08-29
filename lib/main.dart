89b1a887eed4302e10572acc8dfc42e34548fdb1
SayWhoooaaat <erlend.f@live.com>
2024-08-29 16:17:56 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'tier_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rank Everything Always',
+      theme: ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: ColorScheme.dark(
+          primary: Colors.grey[700]!, // buttons n shit
+          inversePrimary: Colors.grey[800]!, // appbar
+        ),
+        scaffoldBackgroundColor: Colors.grey[900]!, // background?
+      ),
+      home: const MyHomePage(title: 'Rank Everything Always'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> _items = [];
+  final Map<String, TierListPage> _tierListPages = {};
+
+  void _addNewItem() async {
+    final String? newItemName = await showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        String? itemName;
+        return AlertDialog(
+          title: const Text('Name Tier List'),
+          content: TextField(
+            onChanged: (value) {
+              itemName = value;
+            },
+            decoration: const InputDecoration(),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Create'),
+              onPressed: () {
+                Navigator.of(context).pop(itemName);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (newItemName != null && newItemName.isNotEmpty) {
+      setState(() {
+        _items.add(newItemName);
+        _tierListPages[newItemName] = TierListPage(title: newItemName);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        padding: const EdgeInsets.all(10),
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => _tierListPages[_items[index]]!,
+                ),
+              );
+            },
+            child: Container(
+              color: Colors.grey[800],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.view_list,
+                    size: 50,
+                    color: Colors.brown,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    _items[index],
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: _items.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addNewItem,
+        tooltip: 'Add new Tier List',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
